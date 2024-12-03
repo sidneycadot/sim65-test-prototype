236cbf9332719302757e8f54268a725c9482ac3e
Sidney Cadot <sidney@jigsaw.nl>
2024-12-03 21:52:13 +0100

    Updated Python scripts for testing the simulator.

--- make_testresult_dashboard.py
+++ make_testresult_dashboard.py
@@ -2,70 +2,90 @@
 
 """Parse the output of a 'test_legal_opcodes.py' run and generate an HTML table summarizing the results."""
 
+import itertools
+import contextlib
 import sys
 import re
 from typing import NamedTuple
-
 
 class Result(NamedTuple):
     error_count: int
     test_count: int
 
-
-def main():
+def parse_file(filename: str):
 
     pattern = re.compile(".*/([0-9a-f]{2})\\.json.*INFO - Test file summary: ([0-9]+) of ([0-9]+) .*", re.ASCII | re.DOTALL)
-
     results = {}
-
     summary_error_count = 0
     summary_test_count = 0
 
-    for line in sys.stdin:
-        match = pattern.fullmatch(line)
-        if match is None:
-            continue
-        opcode = match.group(1)
-        error_count = int(match.group(2))
-        test_count = int(match.group(3))
-        results[opcode] = Result(error_count, test_count)
-        summary_error_count += error_count
-        summary_test_count += test_count
+    with open(filename, "r") as fi:
+        for line in fi:
+            match = pattern.fullmatch(line)
+            if match is None:
+                continue
+            opcode = match.group(1)
+            error_count = int(match.group(2))
+            test_count = int(match.group(3))
+            results[opcode] = Result(error_count, test_count)
+            summary_error_count += error_count
+            summary_test_count += test_count
 
     summary = Result(summary_error_count, summary_test_count)
 
-    print("<html>")
-    print("  <head>")
-    print("    <title>sim65 results for the 65x02 test suite</title>")
-    print("  <head>")
-    print("  <body>")
-    print("    <h1>sim65 results for the 65x02 test suite (10,000 test cases per opcode; failure rate shown)</h1>")
-    print("    <table border=\"1\" style=\"text-align:center\">")
-    for row in range(16):
-        print("      <tr>", end='')
-        for col in range(16):
-            opcode = f"{row*16+col:02x}"
-            result = results.get(opcode)
-            if result is None:
-                print("<td style=\"color:lightgray\">illegal</td>", end='')
-            else:
-                bad_perc = result.error_count / result.test_count * 100.0
-                if bad_perc==0.0:
-                    color="lightgreen"
-                elif bad_perc < 1.0:
-                    color="yellow"
-                elif bad_perc < 100.0:
-                    color="orange"
+    return (results, summary)
+
+def render_results_as_table(sim65_version, sim65_cpu_variant, testcase_directory, results, summary, fo):
+    with contextlib.redirect_stdout(fo):
+        print(f"    <h1>{sim65_version} / {sim65_cpu_variant} / {testcase_directory}</h1>")
+        print("    <table border=\"1\" style=\"text-align:center\">")
+        for row in range(16):
+            print("      <tr>", end='')
+            for col in range(16):
+                opcode = f"{row*16+col:02x}"
+                result = results.get(opcode)
+                if result is None:
+                    print("<td style=\"color:lightgray\">illegal</td>", end='')
                 else:
-                    color="orangered"
-                print(f"<td style=\"background-color:{color}\">0x{opcode}:&nbsp;({bad_perc:.2f}%)</td>", end='')
-        print("</tr>")
-    print("    </table>")
-    print(f"    <p>In total, {summary.error_count} out of {summary.test_count} tests failed ({summary.error_count/summary.test_count*100.0:.3f}%).</h1>")
-    print("  </body>")
-    print("</html>")
+                    bad_perc = result.error_count / result.test_count * 100.0
+                    if bad_perc==0.0:
+                        color="lightgreen"
+                    elif bad_perc < 1.0:
+                        color="yellow"
+                    elif bad_perc < 100.0:
+                        color="orange"
+                    else:
+                        color="orangered"
+                    print(f"<td style=\"background-color:{color}\">0x{opcode}:&nbsp;({bad_perc:.2f}%)</td>", end='')
+            print("</tr>")
+        print("    </table>")
+        print(f"    <p>In total, {summary.error_count} out of {summary.test_count} tests failed ({summary.error_count/summary.test_count*100.0:.3f}%).</h1>")
 
 
-if __name__ == "__main__":
-    main()
+def make_testresult_dashboard(sim65_cpu_variants_and_testcase_directories, html_filename: str):
+
+    with open(html_filename, "w") as fo:
+        with contextlib.redirect_stdout(fo):
+            print("<html>")
+            print("  <head>")
+            print("    <title>sim65 results for the 65x02 test suite</title>")
+            print("  <head>")
+            print("  <body>")
+
+        sim65_versions = ("original", "fixed")
+
+        for (sim65_cpu_variant_and_testcase_directory, sim65_version) in itertools.product(sim65_cpu_variants_and_testcase_directories, sim65_versions):
+            (sim65_cpu_variant, testcase_directory) = sim65_cpu_variant_and_testcase_directory
+            filename = sim65_version + "_" + sim65_cpu_variant + "_" + testcase_directory + ".test-out"
+            filename = filename.replace("/", "-")
+            print(f"Parsing {filename} ...")
+            (results, summary) = parse_file(filename)
+
+            render_results_as_table(sim65_version, sim65_cpu_variant, testcase_directory, results, summary, fo)
+
+        with contextlib.redirect_stdout(fo):
+            print("  </body>")
+            print("</html>")
+
+    print(f"Wrote HTML file: {html_filename}.")
 
--- test_legal_opcodes.py
+++ test_legal_opcodes.py
@@ -5,52 +5,113 @@
 import argparse
 import os
 import subprocess
+import multiprocessing
+import itertools
 
+from make_testresult_dashboard import make_testresult_dashboard
 
-def test_legal_6502_opcodes(version: str, testcase_directory: str) -> None:
-    """Test the 151 vanilla 6502 opcodes that sim65 supports."""
+sim65_supported_opcodes = {
+    "6502": """
+            00 01 .. .. .. 05 06 .. 08 09 0a .. .. 0d 0e ..
+            10 11 .. .. .. 15 16 .. 18 19 .. .. .. 1d 1e ..
+            20 21 .. .. 24 25 26 .. 28 29 2a .. 2c 2d 2e ..
+            30 31 .. .. .. 35 36 .. 38 39 .. .. .. 3d 3e ..
+            40 41 .. .. .. 45 46 .. 48 49 4a .. 4c 4d 4e ..
+            50 51 .. .. .. 55 56 .. 58 59 .. .. .. 5d 5e ..
+            60 61 .. .. .. 65 66 .. 68 69 6a .. 6c 6d 6e ..
+            70 71 .. .. .. 75 76 .. 78 79 .. .. .. 7d 7e ..
+            .. 81 .. .. 84 85 86 .. 88 .. 8a .. 8c 8d 8e ..
+            90 91 .. .. 94 95 96 .. 98 99 9a .. .. 9d .. ..
+            a0 a1 a2 .. a4 a5 a6 .. a8 a9 aa .. ac ad ae ..
+            b0 b1 .. .. b4 b5 b6 .. b8 b9 ba .. bc bd be ..
+            c0 c1 .. .. c4 c5 c6 .. c8 c9 ca .. cc cd ce ..
+            d0 d1 .. .. .. d5 d6 .. d8 d9 .. .. .. dd de ..
+            e0 e1 .. .. e4 e5 e6 .. e8 e9 ea .. ec ed ee ..
+            f0 f1 .. .. .. f5 f6 .. f8 f9 .. .. .. fd fe ..
+            """,
+    "6502X": """
+            00 01 .. 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f
+            10 11 .. 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f
+            20 21 .. 23 24 25 26 27 28 29 2a 2b 2c 2d 2e 2f
+            30 31 .. 33 34 35 36 37 38 39 3a 3b 3c 3d 3e 3f
+            40 41 .. 43 44 45 46 47 48 49 4a 4b 4c 4d 4e 4f
+            50 51 .. 53 54 55 56 57 58 59 5a 5b 5c 5d 5e 5f
+            60 61 .. 63 64 65 66 67 68 69 6a 6b 6c 6d 6e 6f
+            70 71 .. 73 74 75 76 77 78 79 7a 7b 7c 7d 7e 7f
+            80 81 82 83 84 85 86 87 88 89 8a 8b 8c 8d 8e 8f
+            90 91 .. 93 94 95 96 97 98 99 9a 9b 9c 9d 9e 9f
+            a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 aa ab ac ad ae af
+            b0 b1 .. b3 b4 b5 b6 b7 b8 b9 ba bb bc bd be bf
+            c0 c1 c2 c3 c4 c5 c6 c7 c8 c9 ca cb cc cd ce cf
+            d0 d1 .. d3 d4 d5 d6 d7 d8 d9 da db dc dd de df
+            e0 e1 e2 e3 e4 e5 e6 e7 e8 e9 ea eb ec ed ee ef
+            f0 f1 .. f3 f4 f5 f6 f7 f8 f9 fa fb fc fd fe ff
+            """,
+    "65C02": """
+            00 01 02 03 04 05 06 .. 08 09 0a 0b 0c 0d 0e ..
+            10 11 12 13 14 15 16 .. 18 19 1a 1b 1c 1d 1e ..
+            20 21 22 23 24 25 26 .. 28 29 2a 2b 2c 2d 2e ..
+            30 31 32 33 34 35 36 .. 38 39 3a 3b 3c 3d 3e ..
+            40 41 42 43 44 45 46 .. 48 49 4a 4b 4c 4d 4e ..
+            50 51 52 53 54 55 56 .. 58 59 5a 5b 5c 5d 5e ..
+            60 61 62 63 64 65 66 .. 68 69 6a 6b 6c 6d 6e ..
+            70 71 72 73 74 75 76 .. 78 79 7a 7b 7c 7d 7e ..
+            80 81 82 83 84 85 86 .. 88 89 8a 8b 8c 8d 8e ..
+            90 91 92 93 94 95 96 .. 98 99 9a 9b 9c 9d 9e ..
+            a0 a1 a2 a3 a4 a5 a6 .. a8 a9 aa ab ac ad ae ..
+            b0 b1 b2 b3 b4 b5 b6 .. b8 b9 ba bb bc bd be ..
+            c0 c1 c2 c3 c4 c5 c6 .. c8 c9 ca .. cc cd ce ..
+            d0 d1 d2 d3 d4 d5 d6 .. d8 d9 da .. dc dd de ..
+            e0 e1 e2 e3 e4 e5 e6 .. e8 e9 ea eb ec ed ee ..
+            f0 f1 f2 f3 f4 f5 f6 .. f8 f9 fa fb fc fd fe ..
+            """
+}
 
-    legal_6502_opcode_matrix = """
-    00 01 .. .. .. 05 06 .. 08 09 0a .. .. 0d 0e ..
-    10 11 .. .. .. 15 16 .. 18 19 .. .. .. 1d 1e ..
-    20 21 .. .. 24 25 26 .. 28 29 2a .. 2c 2d 2e ..
-    30 31 .. .. .. 35 36 .. 38 39 .. .. .. 3d 3e ..
-    40 41 .. .. .. 45 46 .. 48 49 4a .. 4c 4d 4e ..
-    50 51 .. .. .. 55 56 .. 58 59 .. .. .. 5d 5e ..
-    60 61 .. .. .. 65 66 .. 68 69 6a .. 6c 6d 6e ..
-    70 71 .. .. .. 75 76 .. 78 79 .. .. .. 7d 7e ..
-    .. 81 .. .. 84 85 86 .. 88 .. 8a .. 8c 8d 8e ..
-    90 91 .. .. 94 95 96 .. 98 99 9a .. .. 9d .. ..
-    a0 a1 a2 .. a4 a5 a6 .. a8 a9 aa .. ac ad ae ..
-    b0 b1 .. .. b4 b5 b6 .. b8 b9 ba .. bc bd be ..
-    c0 c1 .. .. c4 c5 c6 .. c8 c9 ca .. cc cd ce ..
-    d0 d1 .. .. .. d5 d6 .. d8 d9 .. .. .. dd de ..
-    e0 e1 .. .. e4 e5 e6 .. e8 e9 ea .. ec ed ee ..
-    f0 f1 .. .. .. f5 f6 .. f8 f9 .. .. .. fd fe ..
-    """
+def test_sim65_supported_opcodes(sim65_cpu_variant_and_testcase_directory, sim65_version: str) -> None:
 
-    testfiles = [os.path.join(testcase_directory, f"{opcode}.json") for opcode in legal_6502_opcode_matrix.split() if opcode != ".."]
+    (sim65_cpu_variant, testcase_directory) = sim65_cpu_variant_and_testcase_directory
 
-    if version == "original":
+    print("Starting test:", sim65_version, sim65_cpu_variant, testcase_directory)
+
+    sim65_supported_opcodes_for_cpu = sim65_supported_opcodes[sim65_cpu_variant]
+
+    testfiles = [os.path.join(testcase_directory, f"{opcode}.json") for opcode in sim65_supported_opcodes_for_cpu.split() if opcode != ".."]
+
+    if sim65_version == "original":
         executable = "./sim65-original-test"
-    elif version == "fixed":
+    elif sim65_version == "fixed":
         executable = "./sim65-fixed-test"
     else:
         raise ValueError()
 
-    subprocess.run([executable, "--cpu-mode=6502"] + testfiles)
+    result = subprocess.run([executable, f"--cpu-mode={sim65_cpu_variant}", "--disable-cycle-count-test"] + testfiles, capture_output=True, encoding='ascii')
 
+    assert result.returncode == 0
+    assert len(result.stderr) == 0
+
+    filename = sim65_version + "_" + sim65_cpu_variant + "_" + testcase_directory + ".test-out"
+    filename = filename.replace("/", "-")
+
+    print("Writing result file:", filename)
+    with open(filename, "w") as fo:
+        fo.write(result.stdout)
 
 def main() -> None:
-    """Test vanilla 6502 opcodes."""
+    """Test 6502 CPU variants against 65x02 instruction behavior descriptions."""
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("version", choices=("original", "fixed"))
-    parser.add_argument("-t", "--testcase-directory", default="65x02/6502/v1")
-    args = parser.parse_args()
+    sim65_cpu_variants_and_testcase_directories = (
+        ("6502", "65x02/6502/v1"),
+        ("6502X", "65x02/6502/v1"),
+        #("65C02", "65x02/rockwell65c02/v1"),
+        #("65C02", "65x02/synertek65c02/v1"),
+        #("65C02", "65x02/wdc65c02/v1"),
+    )
+    sim65_versions = ("original", "fixed")
 
-    test_legal_6502_opcodes(args.version, args.testcase_directory)
+    pool = multiprocessing.Pool()
+    pool.starmap(test_sim65_supported_opcodes, itertools.product(sim65_cpu_variants_and_testcase_directories, sim65_versions))
 
+    make_testresult_dashboard(sim65_cpu_variants_and_testcase_directories, "test_summary.html")
 
 if __name__ == "__main__":
     main()
